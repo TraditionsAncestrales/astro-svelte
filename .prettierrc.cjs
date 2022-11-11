208fae9d4eade45284cfd8fd72733e55dd85c6b7
Gregory Bouteiller <gregory.bouteiller@niama.re>
2022-11-11 10:24:15 +0400

    initialisation

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: false,
+  printWidth: 140,
+  proseWrap: "always",
+  semi: true,
+  singleQuote: true,
+  svelteSortOrder: 'markup-scripts-styles',
+  trailingComma: 'es5',
+};

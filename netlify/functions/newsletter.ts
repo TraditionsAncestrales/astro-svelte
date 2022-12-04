4642a5ebd676371d4ac8087ba448716849ad123d
Gregory Bouteiller <gregory.bouteiller@niama.re>
2022-12-04 08:48:52 +0400

    Update newsletter.ts

--- netlify/functions/newsletter.ts
+++ netlify/functions/newsletter.ts
@@ -1,14 +1,15 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
 import type {Handler} from '@netlify/functions';
 
 export const handler: Handler = async () => {
-  mailchimp.setConfig({apiKey: process.env.MAILCHIMP_API_KEY, server: process.env.MAILCHIMP_SERVER});
-  const res = await (mailchimp as any).ping.get();
+  //mailchimp.setConfig({apiKey: process.env.MAILCHIMP_API_KEY, server: process.env.MAILCHIMP_SERVER});
+  //const res = await (mailchimp as any).ping.get();
 
 	return {
-		statusCode: 302,
-		headers: {
-			"Location": `/?newsletter=${res.health_status}`,
-		},
+		statusCode: 200,
+		body: process.env.MAILCHIMP_API_KEY
+		//statusCode: 302,
+		//headers: {
+			//"Location": `/?newsletter=${res.health_status}`,
+		//},
 	};
 };

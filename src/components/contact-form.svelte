17b125a9c182509405e29ac1b662e7eb8311bcf8
Gregory Bouteiller <gregory.bouteiller@niama.re>
2022-12-05 11:05:22 +0400

    Update contact-form.svelte

--- src/components/contact-form.svelte
+++ src/components/contact-form.svelte
@@ -1,18 +1,18 @@
-<form use:form action="/api/contact" method="post">
+<form action="/api/contact" method="post">
   <input type="text" name="email" />
   <input type="password" name="password" />
   <button type="submit">Sign In</button>
 </form>
 
 <script lang="ts">
-  import {createForm} from 'felte';
+  // import {createForm} from 'felte';
 
-  const {errors, form, touched} = createForm({
-    onError: (error) => {
-      console.debug('error', error);
-    },
-    onSuccess: (response) => {
-      console.debug('success', response);
-    },
-  });
+  // const {errors, form, touched} = createForm({
+  //   onError: (error) => {
+  //     console.debug('error', error);
+  //   },
+  //   onSuccess: (response) => {
+  //     console.debug('success', response);
+  //   },
+  // });
 </script>

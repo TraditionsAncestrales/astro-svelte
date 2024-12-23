feeacc9c3bf58998a2760b7e8d5987ff8ff1dc81
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-12-23 06:57:13 +0400

    up

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { helpersFrom } from "astro-pocketbase";
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware((context, next) => {
-  const { getRecord, getRecords } = helpersFrom({ pocketbase: context.locals.pocketbase, cache: import.meta.env.DEV ? "1d" : undefined });
-  context.locals.getRecord = getRecord;
-  context.locals.getRecords = getRecords;
-  return next();
-});
